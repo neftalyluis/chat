4fc9d540f61b6841a1cb35913b8e1f849fcf6e18
Neftaly Luis <abi@airmail.cc>
2016-05-19 17:07:06 -0500

    Primer Commit

---
+++ src/main/java/mx/lambda/YetAnotherChatApplication.java
@@ -0,0 +1,12 @@
+package mx.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherChatApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/lambda/YetAnotherChatApplicationTests.java
@@ -0,0 +1,18 @@
+package mx.lambda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = YetAnotherChatApplication.class)
+@WebAppConfiguration
+public class YetAnotherChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
